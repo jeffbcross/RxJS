e3cb551f5326a83cdd9729bcf781bf68293f92ed
Jeff Cross <middlefloor@gmail.com>
2015-07-20 17:44:05 -0700

    WIP: start conversion to RX

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,57 @@ var del = require('del');
 var gulp = require('gulp');
 var merge = require('merge-stream');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var typescript = require('typescript');
+var Rx = require('rx');
+var RxNode = require('rx-node');
 
 var OUTPUT_ROOT_DIR = 'dist';
+var optionsObservable = Rx.Observable.just({
+	declarationFiles: true,
+	typescript: typescript
+});
 
 gulp.task('clean/es6', function(done) {
-	del(OUTPUT_ROOT_DIR+'/es6', done);
+	del(OUTPUT_ROOT_DIR + '/es6', done);
 });
 
 gulp.task('clean/cjs', function(done) {
-	del(OUTPUT_ROOT_DIR+'/es5', done);
+	del(OUTPUT_ROOT_DIR + '/cjs', done);
+});
+
+gulp.task('clean/global', function(done) {
+	del(OUTPUT_ROOT_DIR + '/global', done);
 });
 
 gulp.task('build/es6', ['clean/es6'], function(done) {
-	return compileTarget('es6');
+	var observer = new Rx.Observer.create(null, null, done);
+
+	optionsObservable.map(function(opts) {
+		opts.target = 'es6';
+		opts.module = 'es6';
+		return opts;
+	})
+	.map(getTsStream)
+	.flatMap(function(stream) {
+		return writeOutputs({
+			js: stream.js,
+			dts: stream.dts,
+			outputDir: OUTPUT_ROOT_DIR + '/es6'
+		});
+	}).subscribe(observer);
 });
 
-gulp.task('build/cjs', ['clean/cjs'], function(done) {
+gulp.task('build/cjs', ['clean/cjs'], function() {
 	var stream = getTsStream('es6');
-	var jsStream = stream.js.pipe(babel());
-	return writeOutputs(jsStream, stream.dts, OUTPUT_ROOT_DIR + '/cjs');
+	var jsStream = stream.js.pipe(babel({modules: 'common'}));
+	return writeOutputs(jsStream, stream.dts, getOutputDir('commonjs'));
+});
+
+gulp.task('build/global', ['clean/global'], function() {
+	var stream = getTsStream('es6');
+	var jsStream = stream.js.pipe(babel({modules: 'umd'}));
+	return writeOutputs(jsStream, stream.dts, getOutputDir('umd'));
 });
 
 function getOutputDir(moduleType) {
@@ -39,32 +71,19 @@ function getOutputDir(moduleType) {
 			outputDir = OUTPUT_ROOT_DIR + '/global';
 			break;
 		default:
-			outputDir = 'dist/es6'
+			outputDir = OUTPUT_ROOT_DIR + '/es6'
 	}
 	return outputDir;
 }
 
-function getTsStream(outputTarget, moduleType) {
-	var compilerOptions = {
-		target: outputTarget,
-		declarationFiles: true,
-		typescript: require('typescript')
-	};
-	if (moduleType) {
-		compilerOptions.module = moduleType;
-	}
+function getTsStream(compilerOptions) {
 	return gulp.src('src/**/*.ts').
+		pipe(sourcemaps.init()).
 		pipe(tsc(compilerOptions));
 }
 
-function writeOutputs (jsStream, dtsStream, outputDir) {
-	return merge(
-		jsStream.pipe(gulp.dest(outputDir)),
-		dtsStream.pipe(gulp.dest(outputDir)));
-}
-
-function compileTarget(outputTarget, moduleType) {
-	var outputDir = getOutputDir(moduleType);
-	var dualStream = getTsStream(outputTarget, moduleType);
-	return writeOutputs(dualStream.js, dualStream.dts, outputDir);
+function writeOutputs (data) {
+	return RxNode.fromStream(merge(
+		data.js.pipe(gulp.dest(data.outputDir)),
+		data.dts.pipe(gulp.dest(data.outputDir))));
 }
